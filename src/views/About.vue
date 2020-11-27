cb457a9dda74b02a6ebacecf06c7217732523b13
Rodrigo Gimenes <rodrigomacedogimenes@gmail.com>
2020-11-27 17:23:19 -0500

    Todo List app with Vue

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the <strong>TodoList</strong> app with <b>Vue</b>.</p>
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<style scoped>
+  .about {
+    text-align: center;
+    padding: 10px;
+  }
+</style>
